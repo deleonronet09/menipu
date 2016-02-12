8830ff66e74e4df8e7343c5cb38870e5f166d30e
Ronet de Leon <deleonronet09@gmail.com>
2016-02-12 13:24:22 +0800

    updated

--- script.sh
+++ script.sh
@@ -3,5 +3,5 @@
 virtualenv --no-site-packages CI_Venv
 source CI_Venv/bin/activate
 pip install -r development.pip
-pyflakes djangogirls/calculator.py
+pyflakes djangogirls/tests/calculator.py
 figleaf djangogirls/manage.py test djangogirls/tests
