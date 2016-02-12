aa562749a738c1e27682f896affbaa98150995c3
Ronet de Leon <deleonronet09@gmail.com>
2016-02-12 14:37:42 +0800

    update

--- script.sh
+++ script.sh
@@ -4,7 +4,8 @@ virtualenv --no-site-packages CI_Venv
 source CI_Venv/bin/activate
 pip install -r development.pip
 pyflakes djangogirls/tests/calculator.py
-git clone -f https://github.com/ctb/figleaf
+#git clone -f https://github.com/ctb/figleaf
+pip install django==1.9
 python djangogirls/manage.py test djangogirls/tests
 #python ../figleaf/bin/figleaf ../djangogirls/manage.py test ../djangogirls/tests
 #python ../figleaf/bin/figleaf2html .figleaf
