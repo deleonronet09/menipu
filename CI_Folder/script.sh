4c89ece541dc9ec3283a51429ef847fa4748f799
deleonronet09 <[Ddeleonronet09deleonronet-009tet09@gmail.comoutlook.com>
2016-02-22 05:36:44 +0000

    asd

--- CI_Folder/script.sh
+++ CI_Folder/script.sh
@@ -4,6 +4,5 @@ sudo pip install virtualenv
 sudo virtualenv --no-site-packages CI_Venv
 source CI_Venv/bin/activate
 sudo pip install -r CI_Folder/requirements/development.pip
-pip install django
 pyflakes CI_Folder/tests/calculator.py 
 python CI_Folder/manage.py test CI_Folder/tests
