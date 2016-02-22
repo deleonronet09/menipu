092e2381edbc6acd0f2389a7de16c807ae8bfd7a
Ronet de Leon <deleonronet09@outlook.com>
2016-02-22 02:57:45 -0600

    Asd

--- CI_Folder/script.sh
+++ CI_Folder/script.sh
@@ -6,4 +6,4 @@ source CI_Venv/bin/activate
 sudo pip install -r CI_Folder/requirements/development.pip
 pyflakes CI_Folder/tests/calculator.py 
 python CI_Folder/manage.py test CI_Folder/tests
-pip freeze
+sudo -H pip freeze
