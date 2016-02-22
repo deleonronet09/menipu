2ed9bccad399ce13f0918b463ce1857a3d9b4f46
deleonronet09 <[Ddeleonronet09deleonronet-009tet09@gmail.comoutlook.com>
2016-02-22 04:24:25 +0000

    [Dfine

--- CI_Folder/script.sh
+++ CI_Folder/script.sh
@@ -3,6 +3,6 @@
 sudo pip install virtualenv
 sudo virtualenv --no-site-packages CI_Venv
 source CI_Venv/bin/activate
-sudo pip install -r CI_Folder/requirements/development.pip
+pip install -r CI_Folder/requirements/development.pip
 pyflakes CI_Folder/tests/calculator.py 
 python CI_Folder/manage.py test CI_Folder/tests
