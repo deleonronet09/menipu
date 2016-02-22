388a70cf6730d20807983203653a7a8f5b1f5449
Ronet de Leon <deleonronet09@outlook.com>
2016-02-22 03:09:23 -0600

    asd

--- CI_Folder/script.sh
+++ CI_Folder/script.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-sudo pip install virtualenv
 sudo virtualenv --no-site-packages CI_Venv
 source CI_Venv/bin/activate
 sudo pip install -r CI_Folder/requirements/development.pip
 pyflakes CI_Folder/tests/calculator.py 
 python CI_Folder/manage.py test CI_Folder/tests
-sudo pip freeze
+pip freeze
