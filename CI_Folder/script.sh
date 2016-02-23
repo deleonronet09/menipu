a33547d14f0265136b71bf0b6d736bcecd37b5a3
Ronet de Leon <deleonronet09@outlook.com>
2016-02-23 00:22:17 -0600

    asd

--- CI_Folder/script.sh
+++ CI_Folder/script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 sudo pip install -r CI_Folder/requirements/development.pip
-sudo yflakes CI_Folder/tests/calculator.py 
+sudo pyflakes CI_Folder/tests/calculator.py
+sudo flake8 CI_Folder/tests/calculator.py
 sudo python CI_Folder/manage.py test CI_Folder/tests
