c22f3cf0298d0a7b629d9b06004df4ddda723219
Ronet de Leon <deleonronet09@outlook.com>
2016-02-23 00:16:49 -0600

    Remove root ownership

--- CI_Folder/script.sh
+++ CI_Folder/script.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-sudo virtualenv --no-site-packages CI_Venv
-source CI_Venv/bin/activate
 sudo pip install -r CI_Folder/requirements/development.pip
 pyflakes CI_Folder/tests/calculator.py 
 python CI_Folder/manage.py test CI_Folder/tests
-pip freeze
