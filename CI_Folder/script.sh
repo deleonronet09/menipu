97ca53748a5ef211ee6250899718bcbbb21ce5f2
Ronet de Leon <deleonronet09@outlook.com>
2016-02-23 02:56:45 -0600

    coverage2html

--- CI_Folder/script.sh
+++ CI_Folder/script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 sudo pip install -r CI_Folder/requirements/development.pip
-sudo python CI_Folder/manage.py test CI_Folder/tests
-sudo coverage html CI_Folder/manage.py test CI_Folder/tests
+sudo coverage erase
+sudo coverage run CI_Folder/manage.py test CI_Folder/tests
+sudo coverage html
